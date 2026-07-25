37b47cdbfa82bc1a520a743e8c762c1f522b2eff
pankajsaroha <pk89651@gmail.com>
2026-07-25 16:16:43 +0530

    Refactor Dependency Injection and code

--- app/application/application.py
+++ app/application/application.py
@@ -1,4 +1,3 @@
-from app.core.container import ApplicationContainer
 from app.application.banner import StartupBanner
 from app.application.health import HealthChecker
 from app.application.validator import StartupValidator
@@ -6,26 +5,40 @@ from app.application.validator import StartupValidator
 class Application:
     """Application entry point."""
 
-    def __init__(self, container: ApplicationContainer) -> None:
-        self._container = container
-        self._validator = StartupValidator(container.settings)
-        self._health_checker = HealthChecker(container.settings)
-        self._banner = StartupBanner(container.settings)
+    def __init__(
+        self,
+        logger,
+        settings,
+        validator: StartupValidator,
+        banner: StartupBanner,
+        health_checker: HealthChecker
+    ) -> None:
+        self._logger = logger
+        self._settings = settings
+        self._validator = validator
+        self._banner = banner
+        self._health_checker = health_checker
 
     def start(self):
-        logger = self._container.logger
-        settings = self._container.settings
-
         self._validator.validate()
         self._banner.print()
         self._health_checker.check()
 
-        logger.info(
+        self._logger.info(
             "Application started",
-            application_name=settings.app_name,
-            version=settings.app_version,
-            environment=settings.environment
+            application_name=self._settings.app_name,
+            version=self._settings.app_version,
+            environment=self._settings.environment
         )
 
     def stop(self):
-        self._container.Logger.info("Application stopped")
+        self._logger.info("Application stopped")
+
+    def version(self) -> None:
+        print(
+            f"{self._settings.app_name} "
+            f"{self._settings.app_version}"
+        )
+
+    def doctor(self) -> None:  
+        self._health_checker.check()
